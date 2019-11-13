1d2d07c1bce5f85b8fc10f9b9411cacbbdd82633
lguizi <guiziluiz@gmail.com>
2019-11-13 01:54:18 -0300

    Versão 2.0

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 	"html/template"
 )
 
@@ -14,13 +15,34 @@ func main() {
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", index)
+	
+	r := mux.NewRouter()
+	r.HandleFunc("/", index)
+	r.HandleFunc("/listar", listarHandler)
+	r.HandleFunc("/criar", criarHandler)
+	r.HandleFunc("/editar", editarHandler)
+	r.HandleFunc("/deletar", deletarHandler)
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
-
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
 	templates.ExecuteTemplate(w, "index.html", nil)
+}
 
+
+func listarHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "listar.html", nil)
+}
+
+func criarHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "criar.html", nil)
+}
+
+func editarHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "editar.html", nil)
+}
+
+func deletarHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "deletar.html", nil)
 }
